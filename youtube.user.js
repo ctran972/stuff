7c98f25697f08df2408457c81715c1e140ef7f9d
ctran972 <46278176+ctran972@users.noreply.github.com>
2022-02-23 23:31:43 -0800

    Update youtube.user.js

--- youtube.user.js
+++ youtube.user.js
@@ -6,7 +6,9 @@
 
 (function() {
 	'use strict';
-	var css = "#player-container-id { padding-top: 5rem !important; }";
+	var css = "#player-container-id { height:-webkit-fill-available; padding-top:10rem; }\n";
+	   css += "#player { height:inherit; }\n";
+	   css += "video { width:668px; height:376px; left:0px; }\n";
 	var style = document.createElement("style");
 	style.type = "text/css";
 	style.appendChild(document.createTextNode(css));
